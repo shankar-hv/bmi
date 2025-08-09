74416e2f3048614fb2f659580ce352244c8b9032
HV Shankar <vedashankar1617@gmail.com>
2025-08-09 21:44:13 +0530

    bmi.py

---
+++ bmi.py
@@ -0,0 +1,15 @@
+name = input("Enter your name: ").capitalize()
+weight = int(input("Enter your weight in kg: "))
+height = float(input("Enter your height in metre: "))
+
+bmi = weight // (height ** 2)
+print(f"{name}'s Body Mass Index (BMI) is {bmi}.")
+
+if bmi < 18.5:
+    print(f"{name} is underweight.")
+elif bmi < 25:
+    print(f"{name} has normal weight.")
+elif bmi < 30:
+    print(f"{name} is overweight.")
+else:
+    print("Obese!!!")
